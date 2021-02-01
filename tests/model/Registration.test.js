33da242c9f51f10e75bcbc4752435abb8e11bb51
moayada <moayada@kth.se>
2021-02-01 13:26:37 +0100

    added the date of birth validation to validateForm function in model and tested it

--- tests/model/Registration.test.js
+++ tests/model/Registration.test.js
@@ -2,16 +2,21 @@ const Registration = require('../../src/model/Registration');
 
 // Tests for validateForm function.
 test('checks if the first name and last name are valid.', ()=>{
-    expect(Registration.validateForm('Mo@yad', 'Moayad', '$$$')[0].message)
+    expect(Registration.validateForm('Mo@yad', 'Moayad', '$$$', '$$$')[0].message)
     .toBe('Please Enter a valid first name.');
 })
 
 test('checks if the email adress is not valid.', ()=>{
-    expect(Registration.validateForm('Moayad', 'Moayad', '$$$')[0].message)
+    expect(Registration.validateForm('Moayad', 'Moayad', '$$$', '$$$')[0].message)
     .toBe('Please Enter a valid email adress.');
 })
 
-test('checks if the email adress is valid.', ()=>{
-    expect(Registration.validateForm('Moayad', 'Moayad', 'user17@gmail.com').length)
+test('checks if the email adress and the date of birth is valid.', ()=>{
+    expect(Registration.validateForm('Moayad', 'Moayad', 'user17@gmail.com', '20000101').length)
     .toBe(0);
+})
+
+test('checks if the date of birth is not valid.', ()=>{
+    expect(Registration.validateForm('Moayad', 'Moayad', 'user17@gmail.com', '###')[0].message)
+    .toBe('The date of birth should match this format yyyymmdd');
 })
