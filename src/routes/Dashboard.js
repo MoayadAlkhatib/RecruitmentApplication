1c8d03bb9890c196dbe5c9d8dacd33d2ffd99759
moayada <moayada@kth.se>
2021-02-02 09:50:44 +0100

    added validation to the registration UI and fixed the routes

--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -5,7 +5,7 @@ this.Controller = new Controller();
 
  router.get('/', (req,res)=>{ 
    this.Controller.testConnectivity()
-   .then(()=>{ res.status(200).send('Database connected....')
+   .then(()=>{ res.status(200).render('main')
 }).catch(err => {
     res.send('error: '+ err);
 });
@@ -18,9 +18,18 @@ router.get('/registration', (req, res)=>{
 router.post('/registration', (req, res)=>{
   let {firstName, lastName, emailAddress, dateOfBirth, userName,
   Password, repeatPassword} = req.body;
+
+  const err = Controller.validateTheForm(firstName, lastName, emailAddress, 
+    dateOfBirth, userName, Password, repeatPassword);
+
   //console.log(firstName + lastName, emailAddress);
-  console.log(Controller.validateTheForm(firstName, lastName, emailAddress, 
-    dateOfBirth, userName, Password, repeatPassword));
+  //console.log(Controller.validateTheForm(firstName, lastName, emailAddress, 
+  //  dateOfBirth, userName, Password, repeatPassword));
+  if(err.length>0){
+        res.render('registration', {
+          err
+        })
+      }
 })
 
 module.exports = router;
