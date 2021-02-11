1f494a75eaf2a9355e7306214c99c57d9c75ed15
moayada <moayada@kth.se>
2021-02-11 03:54:25 +0100

    added handlebars helper function for years of experiance

--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const Handlebars = require('handlebars');
 
 router.get('/', (req,res)=>{ 
      res.status(200).render('dashboard')
  });
+
+ /**
+  * make options for years of experiance field from 1 to 50.
+  */
+ Handlebars.registerHelper("list", function(context, options) {
+    var ret = "<div>";
+
+    for (let i =1; i <= 50; i++) {
+      ret = ret + "<option value="+i+">" + i +"</option>";
+    }
+
+    return ret + "</div>";
+  });
 
  module.exports = router;
