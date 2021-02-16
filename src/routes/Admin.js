7334302899ea0faa2d1f909172477164a88c5b29
moayada <moayada@kth.se>
2021-02-16 08:17:48 +0100

    display all applicants info in admin view

--- src/routes/Admin.js
+++ src/routes/Admin.js
@@ -5,7 +5,13 @@ let controller = new Controller();
 
 router.get('/', async(req,res)=>{ 
     let applicants= await controller.getAllApplicants();
-    console.log(applicants);
-    res.render('admin', applicants);
+    let applicant=[];
+    //console.log(applicants[0].name);
+    for(let i=0; i<applicants.length; i++){
+        applicant.push({name: applicants[i].name,
+        surname: applicants[i].surname,
+        email: applicants[i].email})
+    }
+    res.render('admin', {applicant: applicant});
  })
  module.exports=router;
