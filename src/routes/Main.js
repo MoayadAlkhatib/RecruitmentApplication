65ec5f0ed75c2d8f96bd37568eca4e31be7f8daa
moayada <moayada@kth.se>
2021-02-16 12:10:18 +0100

    removed unnecessary controller fron main route

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const Controller = require('../controller/Controller');
-this.Controller = new Controller();
 
  router.get('/', (req,res)=>{ 
-   this.Controller.testConnectivity()
-   .then(()=>{ res.status(200).render('main')
-}).catch(err => {
-    res.send('error: '+ err);
-});
+  res.status(200).render('main');
 })
 
 router.get('/logout', (req, res)=>{
