98a77e1a70dc8e7d3a998cec484f440953e141b0
moayada <moayada@kth.se>
2021-02-18 05:28:15 +0100

    added the availability route

--- src/app.js
+++ src/app.js
@@ -29,5 +29,6 @@ app.post('*',Auth.userCheck);
 app.use('/', require('./routes/Main'));
 app.use('/registration', require('./routes/Registration'));
 app.use('/dashboard', Auth.authrequire, require('./routes/Dashboard'));
+app.use('/availability', Auth.authrequire, require('./routes/Availability'));
 app.use('/admin',Auth.adminAuthrequire, require('./routes/Admin'));
 app.use('/login', require('./routes/LogIn'));
