70e7e2cea7a9d957d6073fd849e99a288811e15b
moayada <moayada@kth.se>
2021-02-19 07:20:37 +0100

    passwordEncryption script for migration v2

--- scripts/passwordEncryption.js
+++ scripts/passwordEncryption.js
@@ -3,14 +3,17 @@ require('dotenv').config();
 async function main() {
     // get the client
     const mysql = require('mysql2/promise');
+    
     // create the connection
     const connection = await mysql.createConnection(
         {host:process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB_NAME});
+
     // query database
-    const [rows, fields] = await connection.execute('SELECT * FROM people');
+    const [rows, fields] = await connection.execute(
+        'SELECT id, password FROM people where password IS NOT NULL');
     console.log(rows);
   }
   main();
